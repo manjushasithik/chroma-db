c6feb40ac12e26726640e8d5c9a70d04284febb0
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2024-01-22 14:14:13 +0530

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -15,7 +15,7 @@ import sqlite3
 from langchain_community.vectorstores import Chroma
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import yaml
-from data_loader import load_text
+############################################### from data_loader import load_text
 
 
 # load the config file
@@ -25,7 +25,8 @@ with open("config.yaml", "r") as f:
 # create the open-source embedding function
 def create_vector_db(container_name):
     embedding_function = SentenceTransformerEmbeddings(model_name=config['Model'])
-    data_text = load_text(config['Containers'][f'{container_name}'])
+    data_text = ["abscccc","dddfdfdf']
+    #load_text(config['Containers'][f'{container_name}'])
     vector_db = Chroma.from_documents(documents=data_text, persist_directory="db/", embedding=embedding_function)
     return True
 
