35a6886e181766b61ef6924b07bbafa3ec9ba5b4
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2024-01-23 10:25:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from create_database import create_vector_db
-#from retrieve import retrieve_vector_db
+from retrieve import retrieve_vector_db
 
 import subprocess
 import sys
@@ -29,5 +29,5 @@ app.add_middleware(
 
 @app.get("/chromadb/v1/{query}")
 async def read_item(query: str):
-    return {'data' :'aa' #retrieve_vector_db(query)
+    return {'data' :retrieve_vector_db(query)
            }
