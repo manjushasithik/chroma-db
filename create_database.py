f2e2367949fd08fc67fe14dfc3afa67b7d0c7215
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2024-01-24 09:42:47 +0530

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -10,7 +10,7 @@ from langchain_community.vectorstores import Chroma
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 import yaml
 ############################################### from data_loader import load_text
-
+from data_loader  import  load_pdf
 from data_loader import load_text
 # load the config file
 with open("config.yaml", "r") as f:
@@ -19,11 +19,14 @@ with open("config.yaml", "r") as f:
 # create the open-source embedding function
 def create_vector_db(container_name):
     embedding_function = SentenceTransformerEmbeddings(model_name=config['Model'])
-    #data_text = load_text(config['Containers'][f'{container_name}'])
-    data_text = ["abscccc","dddfdfdf"]
-    vector_db =Chroma.from_texts(data_text, persist_directory="db/", embedding=embedding_function)
+    data_text = load_pdf()
+    #load_text(config['Containers'][f'{container_name}'])
+    # data_text = ["abscccc","dddfdfdf"]
+    # vector_db =Chroma.from_texts(data_text, persist_directory="db/", embedding=embedding_function)
                      
-    #vector_db = Chroma.from_documents(documents=data_text, persist_directory="db/", embedding=embedding_function)
+    vector_db = Chroma.from_documents(documents=data_text, persist_directory="db/", embedding=embedding_function)
     return True
+
+
 
 
